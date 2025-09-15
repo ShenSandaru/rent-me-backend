590c0fb823e9b1d2923b274e2751c3d3de945971
ShenSandaru <136662438+ShenSandaru@users.noreply.github.com>
2025-09-16 00:41:15 +0530

    u

--- app/database.py
+++ app/database.py
@@ -3,11 +3,9 @@ from .config import settings
 from contextlib import asynccontextmanager
 from fastapi import FastAPI
 
-class DataBase:
-    client: AsyncIOMotorClient = None
-    db: AsyncIOMotorDatabase = None
-
-db_connection = DataBase()
+# Create the MongoDB client and database instances
+client = AsyncIOMotorClient(settings.MONGO_DATABASE_URI)
+db = client.get_database("rent_me")
 
 @asynccontextmanager
 async def lifespan(app: FastAPI):
@@ -15,17 +13,15 @@ async def lifespan(app: FastAPI):
     Asynchronous context manager for handling the lifespan of a FastAPI application.
     It connects to the MongoDB database on startup and closes the connection on shutdown.
     """
-    # Connect to MongoDB
-    db_connection.client = AsyncIOMotorClient(settings.MONGO_DATABASE_URI)
-    db_connection.db = db_connection.client.get_database("rent_me")
+    # The connection is already established, just yield
     print("Successfully connected to MongoDB.")
     
     yield
     
     # Close the MongoDB connection
-    db_connection.client.close()
+    client.close()
     print("MongoDB connection closed.")
 
 def get_database() -> AsyncIOMotorDatabase:
     """Returns the database instance."""
-    return db_connection.db
+    return db
