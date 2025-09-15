72213f4741702141763be4ff25211d5142fe6b7b
ShenSandaru <136662438+ShenSandaru@users.noreply.github.com>
2025-09-15 16:23:47 +0530

    Update database.py

--- app/database.py
+++ app/database.py
@@ -2,11 +2,11 @@ import motor.motor_asyncio
 from .config import settings
 
 client = motor.motor_asyncio.AsyncIOMotorClient(settings.MONGO_DATABASE_URI)
-db = client.get_database("rent-me")
+db = client.get_database("rent_me")
 
 # Define separate collections for Users and Owners
-UsersCollection = db.get_collection("users")
-OwnersCollection = db.get_collection("owners")
+UsersCollection = db.get_collection("Users")
+OwnersCollection = db.get_collection("Owners")
 
 # You can also add the items collection here for the next step
 ItemCollection = db.get_collection("items")
